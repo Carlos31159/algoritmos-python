8a9a896fdd03c8fe45fad0be753a973f073febc1
Carlos31159 <25216460@uan.edu.mx>
2026-09-23 01:10:40 -0600

    Refactor problema_11 function for clarity

--- problema11.py
+++ problema11.py
@@ -12,18 +12,3 @@ def problema_11():
  
     print(f"El mayor es: {may}")
  
- 
-# Repaso 12: banquetes "La langosta ahumada"
-def problema_12():
-    per = int(input("Número de personas: "))
- 
-    if per > 300:
-        cp = 75.00
-    elif per > 200:  # más de 200 y hasta 300
-        cp = 85.00
-    else:
-        cp = 95.00
- 
-    pres = per * cp
-    print(f"Costo por platillo: ${cp:.2f}")
-    print(f"Presupuesto total: ${pres:.2f}")
